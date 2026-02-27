3fe7f5b216c1ddebc68ddc8c92e0cd70f6dcc0c4
ingk-124 <32196063+ingk-124@users.noreply.github.com>
2026-02-27 17:05:24 +0900

    update default style settings and fix copyright information in LICENSE

--- src/ingkit/__init__.py
+++ src/ingkit/__init__.py
@@ -1,5 +1,2 @@
 # src/ingkit/__init__.py
-from . import analysis, io, myplot, physics, signals, tools, utils
-
-__all__ = ["analysis", "io", "myplot", "physics", "signals", "tools", "utils"]
 __version__ = "0.1.0"
