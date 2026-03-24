93be89446fc3ac4267501fb3ac6daab97db28321
ingk-124 <32196063+ingk-124@users.noreply.github.com>
2026-03-24 20:07:23 +0900

    fix: update version to 0.2.0 and adjust style usage message

--- src/ingkit/myplot/__init__.py
+++ src/ingkit/myplot/__init__.py
@@ -1,8 +1,8 @@
+# src/ingkit/myplot/__init__.py
 from matplotlib import pyplot
 from . import styles
 from .colormap import get_cmap_with_extreme
 
 styles.use("my_default")
-print(f"Using style: my_default")
 __all__ = ["styles", "get_cmap_with_extreme", "pyplot"]
-__version__ = "0.1.2"
+__version__ = "0.2.0"
